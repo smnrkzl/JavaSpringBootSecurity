f86f2046fb8e063e8a8b07df2626fcaf6a5bd780
smnrkzl <kizilsemanur@gmail.com>
2023-12-07 13:15:59 +0300

    add public endpoint

--- src/main/java/com/controller/PublicController.java
+++ src/main/java/com/controller/PublicController.java
@@ -1,5 +1,6 @@
 package com.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/public")
 public class PublicController {
 
-	
+	   @GetMapping
+	    public String helloWorld() {
+	        return "Hello World! from public endpoint";
+	    }
 	
 }
