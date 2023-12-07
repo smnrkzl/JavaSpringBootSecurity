419062dbc44388a2fdb6370cc62655889d9b4375
smnrkzl <kizilsemanur@gmail.com>
2023-12-07 13:15:15 +0300

    add RestController and RequestController

--- src/main/java/com/controller/PublicController.java
+++ src/main/java/com/controller/PublicController.java
@@ -1,7 +1,10 @@
 package com.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
+@RestController
+@RequestMapping("/public")
 public class PublicController {
 
 	
